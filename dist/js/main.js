3206cf5e95ebc9c35ec1dcf78424b503c5737e19
Andrew Teece <andrewteece@gmail.com>
2023-06-08 17:40:08 -0500

    menu button, javascript & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,37 @@
+// SELECT DOM ITEMS 
+
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu__nav');
+const menuBranding = document.querySelector('.menu__branding');
+const navItem = document.querySelectorAll('.nav__item');
+
+// SET INITIAL STATE OF MENU 
+
+let showMenu = false;
+
+menuBtn.addEventListener('click',  toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItem.forEach(item => item.classList.add('show'));
+
+        //SET MENU STATE 
+
+        showMenu = true;
+    }  else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItem.forEach(item => item.classList.remove('show'));
+
+
+        // SET MENU STATE 
+        showMenu = false;
+    }
+}
